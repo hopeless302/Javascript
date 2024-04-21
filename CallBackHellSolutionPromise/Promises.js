5378762a0a9cc5c13a56e5a73039757eca6ece92
Talha Anwar <mtalhaanwar359@gmail.com>
2024-04-22 00:11:48 +0500

    updating the promise

--- CallBackHellSolutionPromise/Promises.js
+++ CallBackHellSolutionPromise/Promises.js
@@ -1,14 +1,14 @@
-function saveToDB(data) {
-    let internetSpeed = Math.floor(Math.random() * 10 + 1)
-    console.log(internetSpeed);
-    if (internetSpeed > 5) {
-        console.log("You Data is saved in DB", data);
-    } else if (internetSpeed <= 5) {
-        console.log("Your internet is slow");
-    }
-}
+// function saveToDB(data) {
+//     let internetSpeed = Math.floor(Math.random() * 10 + 1)
+//     console.log(internetSpeed);
+//     if (internetSpeed > 5) {
+//         console.log("You Data is saved in DB", data);
+//     } else if (internetSpeed <= 5) {
+//         console.log("Your internet is slow");
+//     }
+// }
 
-saveToDB("My Data");
+// saveToDB("My Data");
 
 //using callback function
 function saveToDbUsingCallBack(data, success, failure) {
@@ -22,9 +22,12 @@ function saveToDbUsingCallBack(data, success, failure) {
 }
 
 saveToDbUsingCallBack("My Data", (data) => {
-    console.log("Your data is saved in DB" , data);
-},
- () => {
-    console.log("Your internet is slow");
-}
+        console.log("Your data is saved in DB", data),
+        saveToDbUsingCallBack("2nd data Saved", () => console.log("Your 2nd data is saved in DB", data),
+                () => console.log("Your 2nd data is not saved in DB", data)
+            )
+    },
+    () => {
+        console.log("Your internet is slow");
+    }
 );
