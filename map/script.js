15dc574da4d0e3dab9807b285ef090b9180fab43
Talha Anwar <mtalhaanwar359@gmail.com>
2024-09-22 17:39:03 +0500

    map 3 parameters

--- map/script.js
+++ map/script.js
@@ -1,5 +1,21 @@
-let arr = [1, 2, 3, 4, 5];
+//Map can Return a completely new array
 
-let duble = arr.map((x) => x*5)
+const numbers = [1, 2, 3, 4, 5];
 
-console.log(duble); // [undefined, undefined, undefined, undefined, undefined]
+const doubled = numbers.map(function(number) {
+  return number * 2;
+});
+
+console.log(doubled); // [2, 4, 6, 8, 10]
+
+
+//In fact map can take up to 3 parameters
+
+const arr = [10, 20, 30, 40];
+
+const result = arr.map((element, index, array) => {
+  console.log(`Element: ${element}, Index: ${index}, Full Array: ${array}`);
+  return element * 2;
+});
+
+console.log(result);  // [20, 40, 60, 80]
