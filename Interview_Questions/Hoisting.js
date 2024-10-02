29c8ecbe8e05e28e5f8b8f657495e3fb58ed27e5
Talha Anwar <mtalhaanwar359@gmail.com>
2024-10-03 00:41:32 +0500

    let, const, var, function expression, class expression in hoisting

--- Interview_Questions/Hoisting.js
+++ Interview_Questions/Hoisting.js
@@ -11,3 +11,16 @@ console.log(hoist); // Output: undefined
 var hoist = 'The variable has been hoisted.';
 
 console.log(hoist); // Output: The variable has been hoisted.
+
+
+//For let and const variables, hoisting does not work.
+
+console.log(hoist); // Output: ReferenceError: hoist is not defined
+let hoist = 'The variable has been hoisted.';
+console.log(hoist); // Output: The variable has been hoisted.
+
+
+//In var its not only declared but also initialized with undefined
+
+
+//Hoisting don't work in function expression and class expression
